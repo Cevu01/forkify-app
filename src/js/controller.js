88a97365dc3b9cc8c7df23e23010626310fd907d
Cevu01 <vuk.v.stankovic01@gmail.com>
2024-09-12 22:02:00 +0200

    Final code

--- src/js/controller.js
+++ src/js/controller.js
@@ -118,9 +118,6 @@ const controlAddRecipe = async function (newRecipe) {
     addRecipeView.renderError(err.message);
   }
 };
-const newFeature = function () {
-  console.log('Welcome to the application!');
-};
 
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
@@ -130,6 +127,5 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  newFeature();
 };
 init();
